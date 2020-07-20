fea1b297cb0589350d3387ce67f36f111d319473
Hafez <hafezusman98@gmail.com>
2020-07-20 19:47:14 +0400

    Clock + Partial Weather API. Need to change API service to accomodate free forecasting...

--- public/script.js
+++ public/script.js
@@ -9,9 +9,9 @@ let displayTime = document.querySelector(".time h1");
 /* Weather API import */
 
 let selectCity = document.querySelector("[data-city-search]");
-const searchBox = new google.maps.places.SearchBox(searchElement);
+const searchBox = new google.maps.places.SearchBox(selectCity);
 
-weather.addListener("places_changed", () => {
+searchBox.addListener("places_changed", () => {
   const place = searchBox.getPlaces()[0];
   if (place == null) {
     return;
@@ -19,19 +19,137 @@ weather.addListener("places_changed", () => {
   let lat = place.geometry.location.lat();
   let lon = place.geometry.location.lng();
 
-  fetch("/weather", {
+  fetch('/weather', {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Accept: "application/json",
+      "Accept": "application/json"
     },
     body: JSON.stringify({
       latitude: lat,
-      longitude: lon,
+      longitude: lon
     }),
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      setWeather(data, place);
-    });
-});
+  }).then(res => res.json()).then(data => {
+      setWeather(data, place.formatted_address);
+      console.log(data);
+    })
+})
+
+const icon = new Skycons({"color":'#ccc'})
+icon.set('icon', 'clear-day')
+
+const statusElem = document.querySelector("[data-status]")
+const locationElem = document.querySelector("[data-location]")
+const windElem = document.querySelector("[data-wind]")
+const temperatureElem = document.querySelector("[data-temperature]")
+const precipitationElem = document.querySelector("[data-precipitation]")
+
+function setWeather(data, place){
+  //console.log("here2")
+  locationElem.textContent = place;
+  statusElem.textContent = data.weather_descriptions[0];
+  temperatureElem.textContent = data.temperature;
+  windElem.textContent = data.wind_speed;
+  precipitationElem.textContent = data.precip + "%";
+  if(data.weather_code===113){
+      icon.set('icon', 'clear-day');
+      icon.play();
+  }
+  else if(data.weather_code===116){
+      icon.set('icon', 'partly-cloudy-day');
+      icon.play();
+  }
+  else  if(data.weather_code===119){
+      icon.set('icon', 'cloudy');
+      icon.play();
+  }
+  else if(data.weather_code===122){
+      icon.set('icon', 'cloudy');
+      icon.play();
+  }
+  else if(data.weather_code===143){
+      icon.set('icon', 'cloudy');
+      icon.play();
+  }
+  else if(data.weather_code===248){
+      icon.set('icon', 'fog');
+      icon.play();
+  }
+  else if(data.weather_code===176){
+      icon.set('icon', 'rain');
+      icon.play();
+  }
+  else if(data.weather_code===179){
+      icon.set('icon', 'snow');
+      icon.play();
+  }
+  else if(data.weather_code===182){
+      icon.set('icon', 'sleet');
+      icon.play();
+  }
+  else if(data.weather_code===200){ //Thundery Outbreaks possible
+      icon.set('icon', 'rain');
+      icon.play();
+  }
+  else if(data.weather_code===227){
+      icon.set('icon', 'wind');
+      icon.play();
+  }
+  else if(data.weather_code===230){
+      icon.set('icon', 'snow');
+      icon.play();
+  }
+  else if(data.weather_code===248){
+      icon.set('icon', 'fog');
+      icon.play();
+  }
+  else if(data.weather_code===260){
+      icon.set('icon', 'fog');
+      icon.play();
+  }
+  else if(data.weather_code===263){
+      icon.set('icon', 'rain');
+      icon.play();
+  }
+  else if(data.weather_code===266){
+      icon.set('icon', 'rain');
+      icon.play();
+  }
+  else if(data.weather_code===281){
+      icon.set('icon', 'sleet');
+      icon.play();
+  }
+  else if(data.weather_code===284){
+      icon.set('icon', 'sleet');
+      icon.play();
+  }
+  else if(data.weather_code===293){
+      icon.set('icon', 'rain');
+      icon.play();
+  }
+  else if(data.weather_code===296){
+      icon.set('icon', 'rain');
+      icon.play();
+  }
+  else if(data.weather_code===299){ 
+      icon.set('icon', 'rain');
+      icon.play();
+  }
+  else if(data.weather_code===302){
+      icon.set('icon', 'rain');
+      icon.play();
+  }
+  else if(data.weather_code===305){
+      icon.set('icon', 'rain');
+      icon.play();
+  }
+  else if(data.weather_code===308){
+      icon.set('icon', 'rain');
+      icon.play();
+  }
+  else if(data.weather_code===311){
+      icon.set('icon', 'sleet');
+      icon.play();
+  }
+  
+}
--- server.js
+++ server.js
@@ -1,4 +1,9 @@
+if (process.env.NODE_ENV!=='production'){
+    require('dotenv').config();
+} 
+
 const WEATHERSTACK_API_KEY = process.env.WEATHERSTACK_API_KEY;
+const OPEN_WEATHER_MAP_API_KEY = process.env.OPEN_WEATHER_MAP_API_KEY;
 
 const axios = require('axios')
 const express = require('express')
@@ -11,14 +16,13 @@ app.use(express.static('public'));
 
 app.post('/weather', (req, res)=>{
     let url = `http://api.weatherstack.com/current?access_key=${WEATHERSTACK_API_KEY}&query=${req.body.latitude},${req.body.longitude}`
-    Axios({
+    //let url = `api.openweathermap.org/data/2.5/forecast/daily?q=Dubai&cnt=AE&appid=${OPEN_WEATHER_MAP_API_KEY}`
+    
+    console.log(req.body.latitude, req.body.longitude)
+    axios({
         url: url,
         responseType: 'json'
-    }).then(data => {res.json(data.data.current); console.log(data.data.current)} )
-})
-
-app.get('/', (req, res)=>{
-    res.render('index')
+    }).then(data => { res.json(data.data.current); console.log(data)})
 })
 
 app.listen(5000, ()=> {
